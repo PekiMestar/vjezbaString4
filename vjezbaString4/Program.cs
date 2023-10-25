721bb1f76f73c8070f96e309ea687467d8cfd89e
PekiMestar <145533879+PekiMestar@users.noreply.github.com>
2023-10-25 09:13:03 +0200

    Add files via upload

---
+++ vjezbaString4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace vjezbaString4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst;
+
+            Console.WriteLine("Upišite neki tekst: ");
+
+            tekst = Console.ReadLine();
+
+            string[] rijec = tekst.Split(' ');
+
+            string zadnja = rijec[rijec.Length - 1];
+
+            string prva = tekst.Substring(0, tekst.IndexOf(' '));
+
+            Console.WriteLine(prva);
+
+            Console.WriteLine(zadnja);
+
+            Console.ReadKey();
+        }
+    }
+}
